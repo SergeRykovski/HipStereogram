13739b2a7450348941322e528cfc633119c311cd
Serge Rykovski <sergey.bebop@gmail.com>
2012-10-18 18:19:04 +0300

    added storyboard with coverflow laceholder and an ugly button

---
+++ HipStereogram/HSMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  HSMainViewController.h
+//  HipStereogram
+//
+//  Created by Serge Rykovski on 10/18/12.
+//  Copyright (c) 2012 Serge Rykovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSMainViewController : UIViewController
+
+@end
---
+++ HipStereogram/HSMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  HSMainViewController.m
+//  HipStereogram
+//
+//  Created by Serge Rykovski on 10/18/12.
+//  Copyright (c) 2012 Serge Rykovski. All rights reserved.
+//
+
+#import "HSMainViewController.h"
+
+@interface HSMainViewController ()
+
+@end
+
+@implementation HSMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
