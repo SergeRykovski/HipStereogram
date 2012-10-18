f30dfa79b5dd99344cb40eba2ae2fc59ab16fa36
Serge Rykovski <sergey.bebop@gmail.com>
2012-10-18 16:43:15 +0300

    Initial Commit

---
+++ HipStereogram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HipStereogram
+//
+//  Created by Serge Rykovski on 10/18/12.
+//  Copyright (c) 2012 Serge Rykovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HipStereogram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HipStereogram
+//
+//  Created by Serge Rykovski on 10/18/12.
+//  Copyright (c) 2012 Serge Rykovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
